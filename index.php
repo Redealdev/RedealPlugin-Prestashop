0123b6cef2f6cf84c999f111dfbc54c47822f1de
Rapson Technologies <38910335+Rapsontechnologies@users.noreply.github.com>
2018-05-07 05:55:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
  *
  * ReDeal
  *
- *  @author    Paragon Kingsley
- *  @copyright 2017 Paragon Kingsley
+ *  @author    Rapson Technology
+ *  @copyright 2018 Rapson Technology
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License ("AFL") v. 3.0
  */
  
