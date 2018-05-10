7b1105ca95ff9a8d24a5c2fbec4f20ef73430e43
Rapson Technologies <38910335+Rapsontechnologies@users.noreply.github.com>
2018-05-10 13:43:47 +0530

    Add files via upload

--- classes/index.php
+++ classes/index.php
@@ -1,1 +1,13 @@
+<?php
+/**
+ * DESCRIPTION.
+ *
+ * ReDeal
+ *
+ *  @author    Redeal
+ *  @copyright 2018 Redeal
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License ("AFL") v. 3.0
+ */
+ 
+//moment
 
