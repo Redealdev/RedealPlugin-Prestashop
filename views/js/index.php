b2283071700a7918ae48d83354f73faec5768228
Rapson Technologies <38910335+Rapsontechnologies@users.noreply.github.com>
2018-05-10 13:46:31 +0530

    Add files via upload

--- views/js/index.php
+++ views/js/index.php
@@ -4,8 +4,8 @@
  *
  * ReDeal
  *
- *  @author    Rapson Technology
- *  @copyright 2018 Rapson Technology
+ *  @author    Redeal
+ *  @copyright 2018 Redeal
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License ("AFL") v. 3.0
  */
  
