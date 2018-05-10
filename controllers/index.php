bf95263f11c3f899df96e169217b3145245fc7f4
Rapson Technologies <38910335+Rapsontechnologies@users.noreply.github.com>
2018-05-10 14:34:41 +0530

    Add files via upload

--- controllers/index.php
+++ controllers/index.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * DESCRIPTION.
- *
- * ReDeal
- *
- *  @author    Redeal
- *  @copyright 2018 Redeal
+ * Plugin Name: Redeal Referral Marketing
+ * Plugin URI: https://www.redeal.se
+ * Description: Redeal Referral Marketing
+ * Version: 1.0
+ * Author: Redeal STHLM AB
+ * Author URI: https://www.redeal.se/en/get-started
+ * License: GPL2
+ *  @copyright 2018 Redeal Referral Marketing
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License ("AFL") v. 3.0
  */
  
