3bc52d64e22e7cbc884c6ddcc64051dc8b3f7494
Rapson Technologies <38910335+Rapsontechnologies@users.noreply.github.com>
2018-05-10 01:12:15 -0700

    Create Redeal.php

---
+++ .idea/inspectionProfiles/Redeal.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * DESCRIPTION.
+ *
+ * RedealPlugin-Prestashop
+ *
+ *  @author    Redeal
+ *  @copyright 2018 Redeal
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License ("AFL") v. 3.0
+ */
+ 
+//moment
